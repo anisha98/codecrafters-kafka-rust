7ade84a7c339ec5c658ec2797643ed999a22e724
Anisha Khetan <anikheta@amazon.com>
2025-09-09 06:13:26 +0000

    Set hardcoded correlation id

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:9092").unwrap(); //9092 port is typically used for kafka
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                
+                // Send hardcoded "7" back to the client
+                stream.write_all(b"7").unwrap();
+                stream.flush().unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
